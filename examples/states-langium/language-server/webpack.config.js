a18e4c05da5e7c79fdc344e2c40366a4418f91c5
Gregory Popov <tordektopor@gmail.com>
2023-02-09 21:58:56 +0000

    Revert "Upgrade & make xtext language server runnable"
    This reverts commit 6ddcb89fb077fa6a9de75fe03368667fa5e4dd8a.

---
+++ examples/states-langium/language-server/webpack.config.js
@@ -0,0 +1,36 @@
+// @ts-check
+const path = require('path');
+
+const outputPath = path.resolve(__dirname, 'out');
+
+/**@type {import('webpack').Configuration}*/
+const config = {
+    target: 'node',
+
+    entry: path.resolve(__dirname, 'src/main.ts'),
+    output: {
+		filename: 'language-server.js',
+        path: outputPath
+    },
+    devtool: 'nosources-source-map',
+
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: ['ts-loader']
+            },
+            {
+                test: /\.js$/,
+                use: ['source-map-loader'],
+                enforce: 'pre'
+            }
+        ]
+    },
+    ignoreWarnings: [/Failed to parse source map/]
+};
+
+module.exports = config;
