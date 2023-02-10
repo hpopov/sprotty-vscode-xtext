b91f8e06cad6835aade93d6f77de8c109d431440
Gregory Popov <tordektopor@gmail.com>
2023-02-10 13:17:02 +0000

    Remove source code of packages

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-    extends: ['./configs/.eslintrc.js'],
-    ignorePatterns: ['**/{node_modules,lib}'],
-    parserOptions: {
-        tsconfigRootDir: __dirname,
-        project: 'tsconfig.json'
-    }
-};
